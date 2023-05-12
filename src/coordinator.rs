690b06ddc211fcebf266fb33659833a8ab03b089
jvogit <justinxvopro@gmail.com>
2023-05-12 01:22:27 -0700

    map phase + rpc

--- src/coordinator.rs
+++ src/coordinator.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BinaryHeap, HashMap, HashSet},
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
     io::{BufRead, BufReader, BufWriter, Write},
     os::unix::net::{UnixListener, UnixStream},
     path::Path,
@@ -8,7 +8,7 @@ use std::{
     time::Instant,
 };
 
-use crate::SOCKET;
+use crate::{coordinator, SOCKET};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 enum Task {
@@ -28,23 +28,39 @@ struct Worker(usize);
 
 pub struct Coordinator {
     M: usize,
-    R: usize,
     workers: HashSet<Worker>,
     workers_keep_alive: HashMap<Worker, Instant>,
     workers_task: HashMap<Worker, Task>,
-    pending_tasks: BinaryHeap<Task>,
+    map_tasks: VecDeque<Task>,
+    reduce_tasks: Vec<VecDeque<Task>>,
 }
 
 impl Coordinator {
+    pub fn R(&self) -> usize {
+        self.reduce_tasks.len()
+    }
+
     pub fn run() {
+        let M = 1;
+        let R = 1;
         let socket_path = Path::new(SOCKET);
         let coordinator = Arc::new(Mutex::new(Coordinator {
-            M: 1,
-            R: 1,
+            M,
             workers: HashSet::new(),
             workers_task: HashMap::new(),
             workers_keep_alive: HashMap::new(),
-            pending_tasks: BinaryHeap::new(),
+            // hard code a map task (test-wc.txt)
+            map_tasks: VecDeque::from([Task::MAP("test-wc.txt".to_string(), TaskStatus::IDLE)]),
+            // hardcode R = 1
+            reduce_tasks: {
+                let mut reduce_tasks = Vec::with_capacity(R);
+
+                for _ in 0..R {
+                    reduce_tasks.push(VecDeque::new());
+                }
+
+                reduce_tasks
+            },
         }));
 
         if socket_path.exists() {
@@ -67,27 +83,24 @@ impl Coordinator {
     fn handle_rpc(this: Arc<Mutex<Self>>, stream: UnixStream) {
         let mut out_stream = BufWriter::new(stream.try_clone().unwrap());
         let mut in_stream = BufReader::new(stream.try_clone().unwrap());
-        let mut id = String::new();
-
-        in_stream.read_line(&mut id).expect("Reading ID failed");
 
         // trim newline after
-        let id = id.trim().parse::<usize>().unwrap();
+        let id = Self::read_line(&mut in_stream)
+            .trim()
+            .parse::<usize>()
+            .expect("id is not usize");
         let worker = Worker(id);
 
-        let mut rpc_call = String::new();
-        in_stream
-            .read_line(&mut rpc_call)
-            .expect("Reading RPC call failed");
+        let rpc_call = Self::read_line(&mut in_stream);
 
         println!("RPC from worker {} : {}", id, rpc_call);
 
-        match &rpc_call[..] {
+        match rpc_call.trim() {
             "register" => {
                 let mut this = this.lock().unwrap();
 
                 if !this.workers.insert(worker) {
-                    panic!("Tried to register exisitng worker!")
+                    panic!("Tried to register existing worker!")
                 }
                 this.workers_task.remove(&worker);
                 this.workers_keep_alive.insert(worker, Instant::now());
@@ -97,28 +110,66 @@ impl Coordinator {
             "steal-work" => {
                 let mut this = this.lock().unwrap();
 
-                if let Some(task) = this.pending_tasks.pop() {
-                    match task {
-                        Task::MAP(at, _) => {
-                            this.workers_task
-                                .insert(worker, Task::MAP(at.clone(), TaskStatus::RUNNING));
-                            this.workers_keep_alive.insert(worker, Instant::now());
-                            Self::write_res(format!("map {}", at), &mut out_stream);
-                        }
-                        Task::REDUCE(at, _) => {
-                            this.workers_task
-                                .insert(worker, Task::REDUCE(at.clone(), TaskStatus::RUNNING));
-                            this.workers_keep_alive.insert(worker, Instant::now());
-                            Self::write_res(format!("reduce {}", at), &mut out_stream);
-                        }
+                this.workers_keep_alive.insert(worker, Instant::now());
+
+                if let Some(Task::MAP(map_at, _)) = this.map_tasks.pop_front() {
+                    Self::write_res(format!("map\n{}\n{}", map_at, this.R()), &mut out_stream);
+                    this.workers_task
+                        .insert(worker, Task::MAP(map_at, TaskStatus::RUNNING));
+                    this.workers_keep_alive.insert(worker, Instant::now());
+                } else if this.reduce_tasks[id].len() == this.M {
+                    // mappers have finished mapping
+                    // its reducing time
+                    let mut reduce_at = String::new();
+
+                    while let Some(Task::REDUCE(r_at, _)) = this.reduce_tasks[id].pop_front() {
+                        reduce_at.push_str(&r_at);
+                        reduce_at.push('\n');
                     }
+
+                    Self::write_res(format!("reduce\n{}", reduce_at.trim()), &mut out_stream);
+                    this.workers_task.insert(
+                        worker,
+                        Task::REDUCE(reduce_at.trim().to_string(), TaskStatus::RUNNING),
+                    );
+                    this.workers_keep_alive.insert(worker, Instant::now());
                 } else {
                     Self::write_res("nowork", &mut out_stream);
                 }
             }
-            "keep-alive" => {
+            "finish" => {
                 let mut this = this.lock().unwrap();
 
+                this.workers_keep_alive.insert(worker, Instant::now());
+
+                match this
+                    .workers_task
+                    .remove(&worker)
+                    .expect("Worker to have task")
+                {
+                    Task::MAP(map_at, _) => {
+                        this.workers_task
+                            .insert(worker, Task::MAP(map_at, TaskStatus::DONE));
+
+                        for r_i in 0..this.R() {
+                            let at = Self::read_line(&mut in_stream);
+                            let at = at.trim().to_string();
+                            this.reduce_tasks[r_i].push_back(Task::REDUCE(at, TaskStatus::IDLE));
+                        }
+                    }
+                    Task::REDUCE(map_at, _) => {
+                        let at = Self::read_line(&mut in_stream);
+                        let at = at.trim().to_string();
+                        println!("Done: {}", at);
+                        this.workers_task
+                            .insert(worker, Task::REDUCE(map_at, TaskStatus::DONE));
+                    }
+                }
+
+                Self::write_res("finish res", &mut out_stream);
+            }
+            "keep-alive" => {
+                let mut this = this.lock().unwrap();
                 this.workers_keep_alive.insert(Worker(id), Instant::now());
 
                 Self::write_res("keep-alive res", &mut out_stream);
@@ -129,6 +180,14 @@ impl Coordinator {
         }
 
         println!("{} Stream closed", id);
+    }
+
+    fn read_line(stream: &mut BufReader<UnixStream>) -> String {
+        let mut res = String::new();
+
+        stream.read_line(&mut res).expect("Read failed!");
+
+        res
     }
 
     fn write_res<S>(res: S, stream: &mut BufWriter<UnixStream>)
--- src/worker.rs
+++ src/worker.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{Read, Write},
+    fs::File,
+    io::{BufRead, BufReader, Read, Write},
     os::unix::net::UnixStream,
     path::Path,
     sync::{Arc, Mutex},
@@ -9,50 +10,150 @@ use std::{
 
 use crate::SOCKET;
 
+enum TaskStatus {
+    IDLE,
+    MAP,
+    REDUCE,
+}
+
 pub struct Worker<'a> {
     id: &'a str,
+    status: TaskStatus,
 }
 
 impl<'a> Worker<'a> {
-    fn map(&mut self, path: &Path) {
+    fn do_map(&mut self, path: &Path, R: usize) {
         // READ FROM PARTITION
+        let lines = Self::read_lines(path).expect("failed to read partition");
+
+        // hardocded mapper function emits word with count of 1
+        fn map(key: &str) -> (String, usize) {
+            (key.to_string(), 1)
+        }
+
         // APPLY MAPPER FUNCTION FROM INITIAL KEY/VALUE TO INTERMEDIATE KEY/VALUE SET (SORTED BY KEY)
+        let lines = lines.map(|s| map(&s.unwrap()));
+        // SCATTER OPERATION BY COMPUTING HASH INDEX AND PUSHING INTERMEDIATE KEY TO DESIGNATED BUCKET
+        let mut chunks: Vec<Vec<(String, usize)>> = vec![vec![]; R];
+        lines.for_each(|(k, v)| {
+            use std::collections::hash_map::DefaultHasher;
+            use std::hash::Hasher;
+
+            let mut hasher = DefaultHasher::new();
+            hasher.write(k.as_bytes());
+
+            let hash_index = hasher.finish() as usize % R;
+
+            chunks[hash_index].push((k, v));
+        });
+
         // EXPORT MAPPING AND NOTIFY COORDINATOR WHEN DONE
+        let output_names = chunks
+            .into_iter()
+            .enumerate()
+            .map(|(i, mut chunk)| {
+                let output_name = format!("{}-{}.txt", self.id, i);
+                let mut output = File::create(&output_name[..]).expect("failed to create output");
+
+                chunk.sort();
+
+                chunk
+                    .into_iter()
+                    .for_each(|(k, v)| writeln!(output, "{},{}", k, v).expect("write failed!"));
+
+                output_name
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        self.do_rpc(format!("finish\n{}", output_names.trim()))
+            .expect("finish call failed!");
+        self.status = TaskStatus::IDLE;
     }
 
-    fn reduce(&mut self, path: &Path) {
+    fn do_reduce(&mut self, path: &Path) {
         // READ SORTED MAPPED PARTITION
+        let lines = Self::read_lines(path).expect("failed to read partition");
         // APPLY REDUCTION
         // EXPORT REDUCTION AND NOTIFY COORDINATOR WHEN DONE
     }
 
     pub fn run(id: &'a str) {
-        let worker = Worker { id };
+        let mut worker = Worker {
+            id,
+            status: TaskStatus::IDLE,
+        };
         worker.do_rpc("register").expect("register failed");
 
         loop {
-            let res = worker.do_rpc("keep-alive").expect("keep-alive rpc failed");
-
+            worker.do_work();
+            worker.do_rpc("keep-alive").expect("keep-alive failed!");
             thread::sleep(Duration::from_secs(10));
         }
     }
 
-    pub fn do_rpc(&self, rpc_call: &str) -> std::io::Result<String> {
+    fn do_work(&mut self) {
+        match self.status {
+            TaskStatus::IDLE => {
+                let res = self.do_rpc("steal-work").expect("steal-work failed");
+                let res: Vec<&str> = res.split('\n').collect();
+
+                match res[0] {
+                    "nowork" => return,
+                    "map" => {
+                        let (at, R) = (res[1], res[2].parse::<usize>().unwrap());
+                        self.status = TaskStatus::MAP;
+                        // TODO: async mapping. For now it is synchronous for testing
+                        self.do_map(&Path::new(at), R);
+                    }
+                    "reduce" => {
+                        self.status = TaskStatus::REDUCE;
+                        
+                        for i in 1..res.len() {
+                            // do it
+                        }
+                    }
+                    _ => {
+                        println!("Unknown res")
+                    }
+                }
+
+                if res[0] == "nowork" {
+                    return;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn do_rpc<S>(&self, rpc_call: S) -> std::io::Result<String>
+    where
+        S: Into<String>,
+    {
+        let rpc_call = rpc_call.into();
         let mut stream = UnixStream::connect(Path::new(SOCKET)).unwrap();
-        
+
         println!("Sending {}", rpc_call);
-        
+
         stream.write(format!("{}\n{}", self.id, rpc_call).as_bytes())?;
-        
+
         stream.shutdown(std::net::Shutdown::Write).unwrap();
 
         let mut res = String::new();
         stream.read_to_string(&mut res)?;
 
-        stream.shutdown(std::net::Shutdown::Both).expect("Shutdown failed");
-        
+        stream
+            .shutdown(std::net::Shutdown::Both)
+            .expect("Shutdown failed");
+
         println!("Received {}", res);
 
         Ok(res)
+    }
+
+    fn read_lines(path: &Path) -> std::io::Result<std::io::Lines<BufReader<File>>> {
+        let file = File::open(path)?;
+
+        Ok(BufReader::new(file).lines())
     }
 }
